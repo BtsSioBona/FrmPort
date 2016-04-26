a970e5483c508c7e3d1ff53bee033a889a7f1442
Sevenn <thomas.s833@gmail.com>
2016-04-26 15:11:10 +0200

    class ToutNavire ok

---
+++ Enhanced/ToutNavire.cs
@@ -0,0 +1,29 @@
+﻿namespace Enhanced
+{
+    class ToutNavire
+    {
+        public ToutNavire(int noLloyds, string nomNavire, string pavillon, int longeur, int largeur, int tirantEau)
+        {
+            NoLloyds = noLloyds;
+            NomNavire = nomNavire;
+            Pavillon = pavillon;
+            Longeur = longeur;
+            Largeur = largeur;
+            TirantEau = tirantEau;
+        }
+
+        public int NoLloyds { get; set; }
+
+        public string NomNavire { get; set; }
+
+        public string Pavillon { get; set; }
+
+        public int Longeur { get; set; }
+
+        public int Largeur { get; set; }
+
+        public int TirantEau { get; set; }
+
+        public static int NbToutNavires { get; set; }
+    }
+}
