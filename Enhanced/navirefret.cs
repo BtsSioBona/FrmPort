ff873b72d76be413a45e93eadd69301e2f34d4ed
Sevenn <thomas.s833@gmail.com>
2016-04-26 15:18:56 +0200

    Ajout de la classe NavireFret

---
+++ Enhanced/navirefret.cs
@@ -0,0 +1,18 @@
+﻿namespace Enhanced
+{
+    public class NavireFret : ToutNavire
+    {
+        public NavireFret(string libelleFret, int qteFret, int noLloyds, string nomNavire, string pavillon, int longeur, int largeur, int tirantEau)
+            : base(noLloyds, nomNavire, pavillon, longeur, largeur, tirantEau)
+        {
+            LibelleFret = libelleFret;
+            QteFret = qteFret;
+        }
+
+        public string LibelleFret { get; set; }
+
+        public int QteFret { get; set; }
+
+        public static int NbNavireFret { get; set; }
+    }
+}
