8516f4a98f290a722fc40b2641f9dec5f0b4c158
Sevenn <thomas.s833@gmail.com>
2016-04-26 16:07:53 +0200

    creation des prototype de fonctions

--- Enhanced/Port.cs
+++ Enhanced/Port.cs
@@ -1,10 +1,13 @@
-﻿using static Enhanced.NavireFret;
+﻿using System.Collections.ObjectModel;
+using static Enhanced.NavireFret;
 using static Enhanced.NavirePassager;
 
 namespace Enhanced
 {
     public class Port
     {
+        private Collection<ToutNavire> TousLesNaviresPort;
+
         public Port(string nomPort)
         {
             NomPort = nomPort;
@@ -25,6 +28,43 @@ namespace Enhanced
         public int NbNavireTotal()
         {
             return NbNavirePassager + NbNavireFret;
+        }
+
+        /// <summary>
+        /// Créer un nouveau navire de type fret
+        /// </summary>
+        /// <param name="pUnNavireFret">Navire fret à ajouter</param>
+        public void AjouterNavire(NavireFret pUnNavireFret)
+        {
+            
+        }
+
+        /// <summary>
+        /// Créer un nouveau navire pouvant accueillir des passagers
+        /// </summary>
+        /// <param name="pUnNavirePassager">Navire passager à ajouter</param>
+        public void AjouterNavire(NavirePassager pUnNavirePassager)
+        {
+            
+        }
+        
+        /// <summary>
+        /// Supprime un navire à l'indice i
+        /// </summary>
+        /// <param name="i">Indice</param>
+        public void SupprimerNavire(int i)
+        {
+            
+        }
+
+        /// <summary>
+        /// Retoure le navire à l'indice i
+        /// </summary>
+        /// <param name="i">indice</param>
+        /// <returns>Navire fret ou passager</returns>
+        public ToutNavire ExtraireNavire(int i)
+        {
+            
         }
     }
 }
