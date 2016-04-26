0c05003dd6f8dbc99b8bef235cf0940b72aaad02
Sevenn <thomas.s833@gmail.com>
2016-04-26 15:27:45 +0200

    Ajout de la classe NavirePassager

---
+++ Enhanced/NavirePassager.cs
@@ -0,0 +1,21 @@
+﻿namespace Enhanced
+{
+    class NavirePassager : ToutNavire
+    {
+        public NavirePassager(int nbMaxiPassager, int nbChambres, int nbCouchettes, int noLloyds, string nomNavire, string pavillon, int longeur, int largeur, int tirantEau)
+            : base(noLloyds, nomNavire, pavillon, longeur, largeur, tirantEau)
+        {
+            NbMaxiPassager = nbMaxiPassager;
+            NbChambres = nbChambres;
+            NbCouchettes = nbCouchettes;
+        }
+
+        public int NbMaxiPassager { get; set; }
+
+        public int NbChambres { get; set; }
+
+        public int NbCouchettes { get; set; }
+
+        public static int NbNavirePassager { get; set; }
+    }
+}
